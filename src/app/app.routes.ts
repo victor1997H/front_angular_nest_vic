7aa1f8c3ac3be97b77f42f812e8015abafe48c45
victor1997H <vdy.hualpa@yavirac.edu.ec>
2025-11-13 23:13:17 -0500

    Actualizo rutas de admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "admin",
+    loadChildren: () =>
+      import("./admin/admin.module").then(m => m.AdminModule)
+  }
+];
